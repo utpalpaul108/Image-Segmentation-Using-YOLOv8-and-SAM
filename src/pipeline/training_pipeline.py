18544a46cf05a6acbc69c5d51fdfa6a3ba72be65
Utpal Paul <utpalseu@gmail.com>
2024-06-21 15:27:19 +0600

    MLflow Added

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -2,22 +2,38 @@ from ultralytics import YOLO
 import torch
 import shutil
 import os
+import mlflow
 
 
 class TrainingPipeline:
     def train(self):
-        try:
-            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-            model = YOLO("yolov8x.pt")
-            model.train(data="data.yaml", epochs=100, imgsz=640, device=device)
-            
-            # Check if the folder exists
-            if os.path.exists('runs/detect/train/weights/best.pt'):
-                os.makedirs('artifacts/model', exist_ok=True)
-                shutil.copy('runs/detect/train/weights/best.pt', 'artifacts/model')
+        # Start an MLflow run
+        with mlflow.start_run():
+            try:
+                device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+                model = YOLO("yolov8x.pt")
+                results = model.train(data="data.yaml", epochs=100, imgsz=640, device=device)
 
-            if os.path.exists('runs'):
-                shutil.rmtree('runs')
+                # MLflow Log parameters
+                mlflow.log_param("epochs", 100)
+                mlflow.log_param("imgsz", 640)
+                mlflow.log_param("device", device)
 
-        except Exception as e:
-            raise e
+                # Log metrics
+                mlflow.log_metric('train_loss', results[-1].metrics['train_loss'])
+                mlflow.log_metric('val_loss', results[-1].metrics['val_loss'])
+                mlflow.log_metric('mAP', results[-1].metrics['mAP50'])
+                
+                # Check if the folder exists
+                model_path = os.path.join('runs','detect','train','weights', 'best.pt')
+                if os.path.exists(model_path):
+                    # Log the model
+                    mlflow.log_artifact(model_path)
+                    os.makedirs('artifacts/model', exist_ok=True)
+                    shutil.copy(model_path, 'artifacts/model')
+
+                if os.path.exists('runs'):
+                    shutil.rmtree('runs')
+
+            except Exception as e:
+                raise e
