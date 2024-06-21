6b7acdf72ce81fa6a3970f818a46522f52e44e03
Utpal Paul <utpalseu@gmail.com>
2024-06-21 15:55:22 +0600

    MlFlow Modified

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -7,9 +7,10 @@ import mlflow
 
 class TrainingPipeline:
     def train(self):
-        # Start an MLflow run
-        with mlflow.start_run():
-            try:
+        
+        try:
+            # Start an MLflow run
+            with mlflow.start_run():
                 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
                 model = YOLO("yolov8x.pt")
                 results = model.train(data="data.yaml", epochs=100, imgsz=640, device=device)
@@ -20,20 +21,23 @@ class TrainingPipeline:
                 mlflow.log_param("device", device)
 
                 # Log metrics
-                mlflow.log_metric('train_loss', results[-1].metrics['train_loss'])
-                mlflow.log_metric('val_loss', results[-1].metrics['val_loss'])
-                mlflow.log_metric('mAP', results[-1].metrics['mAP50'])
+                # mlflow.log_metric('train_loss', results[-1].metrics['train_loss'])
+                # mlflow.log_metric('val_loss', results[-1].metrics['val_loss'])
+                # mlflow.log_metric('mAP', results[-1].metrics['mAP50'])
                 
                 # Check if the folder exists
                 model_path = os.path.join('runs','detect','train','weights', 'best.pt')
                 if os.path.exists(model_path):
                     # Log the model
-                    mlflow.log_artifact(model_path)
+                    # mlflow.log_artifact(model_path)
                     os.makedirs('artifacts/model', exist_ok=True)
                     shutil.copy(model_path, 'artifacts/model')
 
                 if os.path.exists('runs'):
                     shutil.rmtree('runs')
 
-            except Exception as e:
-                raise e
+        except Exception as e:
+            print(f"An error occurred during training: {e}")
+            mlflow.end_run(status='FAILED')
+            raise
+            # raise e
